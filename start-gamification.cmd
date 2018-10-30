bad3904ec488fb1ca93708c2f677aefba27f966a
sergey.kulagin <sergey.kulagin@helmes.ee>
2018-10-30 18:14:45 +0300

    [Multiplication UI]: 1) update cmd scripts

--- start-gamification.cmd
+++
@@ -1,1 +0,0 @@
-java -jar ..\Gamification\target\gamification-0.0.1-SNAPSHOT.jar
---
+++ start-ms.cmd
@@ -0,0 +1,3 @@
+title MULTIPLICATION-UI-MS
+start java -jar ..\Gamification\target\gamification-0.0.1-SNAPSHOT.jar
+start java -jar ..\SocialMultiplication\target\social-multiplication-0.0.1-SNAPSHOT.jar
