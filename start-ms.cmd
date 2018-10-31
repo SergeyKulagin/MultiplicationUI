a1ea9c850fc0776d407856998ebb365eb90aed3f
sergey.kulagin <sergey.kulagin@helmes.ee>
2018-10-31 18:16:57 +0300

    [Multiplication UI]: 1) more MS in the script 2) start MS in the same window

--- start-ms.cmd
+++ start-ms.cmd
@@ -1,3 +1,4 @@
 title MULTIPLICATION-UI-MS
-start java -jar ..\Gamification\target\gamification-0.0.1-SNAPSHOT.jar
-start java -jar ..\SocialMultiplication\target\social-multiplication-0.0.1-SNAPSHOT.jar
+start /B java -jar ..\DiscoveryService\target\discoveryservice-0.0.1-SNAPSHOT.jar
+start /B java -jar ..\Gamification\target\gamification-0.0.1-SNAPSHOT.jar
+start /B java -jar ..\SocialMultiplication\target\social-multiplication-0.0.1-SNAPSHOT.jar
