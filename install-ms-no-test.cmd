b6803fe85670dce9bdd014831822615c8d31423e
sergey.kulagin <sergey.kulagin@helmes.ee>
2018-11-15 13:30:45 +0300

    [Multiplication UI]: - update start-ms  - add deploy ms script

---
+++ install-ms-no-test.cmd
@@ -0,0 +1,4 @@
+call mvn -f ..\DiscoveryService\pom.xml clean install -DskipTests
+call mvn -f ..\Gateway\pom.xml clean install -DskipTests
+call mvn -f ..\Gamification\pom.xml clean install -DskipTests
+call mvn -f ..\SocialMultiplication\pom.xml clean install -DskipTests
--- start-ms.cmd
+++ start-ms.cmd
@@ -1,6 +1,7 @@
 title MULTIPLICATION-UI-MS
 
-start /B java -jar ..\DiscoveryService\target\discoveryservice-0.0.1-SNAPSHOT.jar --server.port=9999
-start /B java -jar ..\Gateway\target\gateway-0.0.1-SNAPSHOT.jar --server.port=8000
+start /B java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8005 -jar ..\Gateway\target\gateway-0.0.1-SNAPSHOT.jar --server.port=8000
+start /B java -jar ..\DiscoveryService\target\discoveryservice-0.0.1-SNAPSHOT.jar --server.port=9999 
 start /B java -jar ..\Gamification\target\gamification-0.0.1-SNAPSHOT.jar --server.port=8099
 start /B java -jar ..\SocialMultiplication\target\social-multiplication-0.0.1-SNAPSHOT.jar --server.port=8097
+start /B java -jar ..\SocialMultiplication\target\social-multiplication-0.0.1-SNAPSHOT.jar --server.port=8100 --logging.file=logs/multiplication2.txt
